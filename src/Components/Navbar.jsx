4df6c6acc27e805350481ba90d25cf5e36c96d20
rudra-123-patel <rudrapatel1054@gmail.com>
2025-06-17 12:01:25 +0530

    updated Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 const navLinks = [
@@ -10,6 +10,7 @@ const navLinks = [
 
 const Navbar = () => {
   motion;
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <>
@@ -17,14 +18,15 @@ const Navbar = () => {
         initial={{ opacity: 0, y: -100 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.2, duration: 0.3 }}
-        className="navbar fixed top-0 left-0 w-full sm:w-full  z-50 flex px-7 py-6 justify-between items-center backdrop-blur-sm"
+        className="navbar fixed top-0 left-0 w-full sm:w-full z-50 flex px-7 py-6 justify-between items-center backdrop-blur-sm"
         style={{ WebkitBackdropFilter: "blur(12px)" }}
       >
+        {/* Logo */}
         <motion.div
           initial={{ opacity: 0, y: -100 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ delay: 0.2, duration: 0.3 }}
-          className="div-text "
+          className="div-text"
         >
           <a
             href="#"
@@ -33,9 +35,30 @@ const Navbar = () => {
             RUDRA
           </a>
         </motion.div>
-        {/* Desktop Nav */}
+
+        {/*  Menu button for mobile */}
+        <div
+          onClick={() => setIsOpen((prev) => !prev)}
+          className="sm:hidden z-50 relative ml-auto"
+        >
+          {isOpen ? (
+            <img
+              className="invert brightness-0 z-999 relative w-8 h-8"
+              src="/image/close-fill.png"
+              alt="close"
+            />
+          ) : (
+            <img
+              className="invert brightness-0 z-999 relative w-8 h-8"
+              src="/image/menu-3-line.png"
+              alt="menu"
+            />
+          )}
+        </div>
+
+        {/* Desktop Menu */}
         <nav>
-          <ul className="hidden sm:flex gap-8 ">
+          <ul className="hidden sm:flex gap-8">
             {navLinks.map((link, idx) => (
               <motion.li
                 key={link.name}
@@ -45,7 +68,7 @@ const Navbar = () => {
               >
                 <a
                   href={link.href}
-                  className="text-3xl text-white hover:text-[#f24545] transition-colors duration-200 border-b-2 border-transparent hover:border-[#f24545] "
+                  className="text-3xl text-white hover:text-[#f24545] transition-colors duration-200 border-b-2 border-transparent hover:border-[#f24545]"
                 >
                   {link.name}
                 </a>
@@ -54,15 +77,36 @@ const Navbar = () => {
           </ul>
         </nav>
       </motion.div>
-      {/* Mobile Nav */}
 
-      <motion.div
-        initial={{ x: "100%" }}
-        animate={{ x: 0 }}
-        exit={{ x: "100%" }}
-        transition={{ duration: 0.3 }}
-        className="fixed top-0 right-0 h-full bg-[#181818] z-50 flex flex-col items-center pt-24 sm:hidden"
-      ></motion.div>
+      {/* Mobile Menu */}
+      {isOpen && (
+        <motion.div
+          initial={{ x: "100%" }}
+          animate={{ x: 0 }}
+          exit={{ x: "100%" }}
+          transition={{ duration: 0.3 }}
+          className="fixed top-0 right-0 h-full w-32 bg-[#f24545] z-40 flex flex-col items-center pt-24 sm:hidden"
+        >
+          <ul className="flex flex-col text-center gap-6">
+            {navLinks.map((link, idx) => (
+              <motion.li
+                key={link.name}
+                initial={{ x: 50, opacity: 0 }}
+                animate={{ x: 0, opacity: 1 }}
+                transition={{ delay: 0.3 + idx * 0.1, duration: 0.3 }}
+              >
+                <a
+                  onClick={() => setIsOpen(false)}
+                  href={link.href}
+                  className="text-2xl text-gray-200 hover:text-[black] transition-colors duration-200"
+                >
+                  {link.name}
+                </a>
+              </motion.li>
+            ))}
+          </ul>
+        </motion.div>
+      )}
     </>
   );
 };
